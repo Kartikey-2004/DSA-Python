0418d4b7f252bec24b245749f955105d680e26e2
Kartikey Gupta <kartikey.2004@icloud.com>
2026-01-31 12:35:31 +0530

    modified:   Leetcode/1.py 	new file:   Leetcode/136.py

--- Leetcode/1.py
+++ Leetcode/1.py
@@ -1,8 +1,11 @@
+from typing import List
+
+
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
+            need = target - num
+            if need in seen:
+                return [seen[need], i]
             seen[num] = i
