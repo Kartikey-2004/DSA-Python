28912710ec23f6c0dd6caea47941825082c7dfb3
Kartikey Gupta <kartikey.2004@icloud.com>
2026-06-01 21:05:41 +0530

    new file:   Leetcode/121.py 	new file:   Leetcode/169.py 	new file:   Leetcode/1838.py 	new file:   Leetcode/48.py 	new file:   Leetcode/53.py

---
+++ Leetcode/1838.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l = 0
+        total = 0
+        res = 0
+        for r in range(len(nums)):
+            total += nums[r]
+
+            while (r - l + 1) * nums[r] - total > k:
+                total -= nums[l]
+                l += 1
+
+            res = max(res, r - l + 1)
+
+        return res
---
+++ test.py
@@ -0,0 +1,6 @@
+def func(*args, **kwargs):
+    print("args:", args)
+    # print("kwargs:", kwargs)
+
+
+func(1, 2, 3, x=10, y=20)
