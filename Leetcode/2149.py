cb9b2ad80e58fe1fd404d62c6f5181c798715ab1
Kartikey Gupta <kartikey.2004@icloud.com>
2026-06-01 21:24:34 +0530

    new file:   Leetcode/2149.py 	new file:   Leetcode/31.py 	new file:   Leetcode/73.py 	new file:   Leetcode/75.py

---
+++ Leetcode/2149.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+
+        pos_index = 0
+        neg_index = 1
+
+        for num in nums:
+            if num > 0:
+                res[pos_index] = num
+                pos_index += 2
+            else:
+                res[neg_index] = num
+                neg_index += 2
+
+        return res
